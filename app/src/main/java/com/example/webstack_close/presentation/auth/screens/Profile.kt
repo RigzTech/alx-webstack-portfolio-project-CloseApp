96800c1e9380afcd5c83d22e6b9d6cb2ca13e0c7
wallacesintra <wallvxwallace962@gmail.com>
2024-09-18 19:56:57 +0300

    auth presentation

---
+++ app/src/main/java/com/example/webstack_close/presentation/auth/screens/Profile.kt
@@ -0,0 +1,26 @@
+package com.example.close.presentation.auth.screens
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import com.example.close.data.users.models.CloseUserData
+
+@Composable
+fun Profile(
+    user: CloseUserData,
+    signOut: () -> Unit = {}
+){
+    Column {
+        Text(text = "User Details")
+//        user.email?.let { Text(text = it) }
+//        Text(text = user.providerData.toString())
+        Text(text = user.username)
+        
+        Text(text = user.uid)
+
+        Button(onClick = signOut) {
+            Text(text = "Sign out")
+        }
+    }
+}
