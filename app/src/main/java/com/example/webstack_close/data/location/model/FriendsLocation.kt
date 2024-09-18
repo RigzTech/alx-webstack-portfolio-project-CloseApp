994aeb8ea26e96a14475644bcb89f4e1636e3c59
root <Rigztech>
2024-09-18 19:18:23 +0000

    app model

---
+++ app/src/main/java/com/example/webstack_close/data/location/model/FriendsLocation.kt
@@ -0,0 +1,8 @@
+package com.example.close.data.location.model
+
+data class FriendsLocation(
+    val friendUID: String = "",
+    val friendsLocationList: List<FriendLocationDetail> = emptyList()
+)
+
+
