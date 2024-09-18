9dcf41a775de0b21ee29fae7f93aa7293e7f38ed
root <Rigztech>
2024-09-18 11:54:09 +0000

    Auth Result

--- app/src/main/java/com/example/webstack_close/data/auth/AuthResult.kt
+++ app/src/main/java/com/example/webstack_close/data/auth/AuthResult.kt
@@ -1,64 +1,7 @@
-
-ckage com.example.close.data.auth
-
-
-sealed class Resource<T>(val data: T? = null, val message: String? = null) {
-    class Success<T>(data: T?) : Resource<T>(data)
-    class Error<T>(message: String, data: T? = null) : Resource<T>(data, message)
-}
-ckage com.example.close.data.auth
+package com.example.close.data.auth
 
 
 sealed class Resource<T>(val data: T? = null, val message: String? = null) {
     class Success<T>(data: T?) : Resource<T>(data)
     class Error<T>(message: String, data: T? = null) : Resource<T>(data, message)
 }
-ckage com.example.close.data.auth
-
-
-sealed class Resource<T>(val data: T? = null, val message: String? = null) {
-    class Success<T>(data: T?) : Resource<T>(data)
-    class Error<T>(message: String, data: T? = null) : Resource<T>(data, message)
-}
-ckage com.example.close.data.auth
-
-
-sealed class Resource<T>(val data: T? = null, val message: String? = null) {
-    class Success<T>(data: T?) : Resource<T>(data)
-    class Error<T>(message: String, data: T? = null) : Resource<T>(data, message)
-}
-ckage com.example.close.data
-
-import android.content.Context
-import com.example.close.data.auth.UserDataSource
-import com.example.close.data.cometChat.CometChatAuthImp
-import com.example.close.data.users.CloseUserDataSource
-import com.example.close.data.location.LocationDataSource
-import com.example.close.data.messaging.CloseMessagingDataSource
-import com.google.android.gms.location.FusedLocationProviderClient
-import com.google.android.gms.location.LocationRequest
-import com.google.android.gms.location.LocationServices
-import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.firestore.FirebaseFirestore
-
-interface AppContainer {
-    val userDataSource: UserDataSource
-    val closeUserDataSource: CloseUserDataSource
-    val locationDataSource: LocationDataSource
-    val cometChatAuthImp: CometChatAuthImp
-    val closeMessagingDataSource: CloseMessagingDataSource
-}
-
-class DefaultContainer(
-    private val applicationContext: Context,
-    private val auth: FirebaseAuth,
-    private val firestoreDb: FirebaseFirestore,
-    private val cometAppID: String,
-    private val cometRegion: String,
-    private val cometRestApiKey: String,
-
-): AppContainer {
-
-    private val fusedLocationClient: FusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(applicationContext)
-
-
