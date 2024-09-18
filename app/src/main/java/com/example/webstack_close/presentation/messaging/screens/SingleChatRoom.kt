29f9a97073a028ad65cc3648706eef8f5989dfc4
wallacesintra <wallvxwallace962@gmail.com>
2024-09-18 20:01:32 +0300

    messaging features

---
+++ app/src/main/java/com/example/webstack_close/presentation/messaging/screens/SingleChatRoom.kt
@@ -0,0 +1,165 @@
+package com.example.close.presentation.messaging.screens
+
+import android.os.Build
+import androidx.annotation.RequiresApi
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.lazy.rememberLazyListState
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.Send
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.IconButtonDefaults
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import com.example.close.R
+import com.example.close.presentation.components.LargeText
+import com.example.close.presentation.components.MediumText
+import com.example.close.presentation.messaging.components.ChatBubble
+import com.example.close.presentation.messaging.viewmodel.SingleChatRoomViewModel
+
+@RequiresApi(Build.VERSION_CODES.O)
+@Composable
+fun SingleChatRoom(
+    chatRoomUid: String,
+    currentUserUid: String,
+//    messagingViewModel: MessagingViewModel,
+    singleChatRoomViewModel: SingleChatRoomViewModel
+) {
+//    val messageText by messagingViewModel.messageText.collectAsState()
+    val messageText by singleChatRoomViewModel.messageText.collectAsState()
+
+//    val messages by messagingViewModel.messages.collectAsState()
+    val messages by singleChatRoomViewModel.messages.collectAsState()
+
+    val listState = rememberLazyListState()
+
+    LaunchedEffect(key1 = true) {
+        singleChatRoomViewModel.setChatRoomUID(chatRoomUID = chatRoomUid)
+        listState.scrollToItem(index = 0)
+    }
+
+    val showMessageLIst by singleChatRoomViewModel.showMessageList.collectAsState()
+
+    Box(modifier = Modifier.fillMaxHeight()) {
+        Column(
+            modifier = Modifier
+                .padding(10.dp)
+                .fillMaxWidth()
+        ) {
+
+            LargeText(
+                text = stringResource(id = R.string.messages),
+                modifier = Modifier.padding(vertical = 10.dp)
+            )
+
+            
+            if (showMessageLIst.messageList.isEmpty()){
+                MediumText(
+                    text = stringResource(id = R.string.say_hi),
+                    modifier = Modifier
+                        .weight(1.0f)
+                        .align(Alignment.CenterHorizontally)
+                )
+            }
+            LazyColumn(
+                state = listState,
+                reverseLayout = true,
+                modifier = Modifier
+                    .weight(1.0f)
+                    .padding(bottom = 70.dp)
+            ) {
+                items(
+                    items = showMessageLIst.messageList,
+//                    items = messages,
+                    key = { message -> message.messageUid }
+                ){ message ->
+                    ChatBubble(
+                        currentUserUid = currentUserUid,
+                        messageUI = message,
+                        deleteMessage = {
+                            singleChatRoomViewModel.deleteMessage(roomUid = chatRoomUid, message = message)
+//                            messagingViewModel.deleteMessage(roomUid = chatRoomUid, message = message)
+                        }
+                    )
+                }
+            }
+            
+        }
+
+        Box(
+            modifier = Modifier
+                .padding(5.dp)
+                .align(Alignment.BottomCenter)
+                .fillMaxWidth()
+        ) {
+            TextField(
+                value = messageText,
+                placeholder = { Text(text = stringResource(id = R.string.message))},
+                shape = RoundedCornerShape(40.dp),
+                colors = TextFieldDefaults.colors(
+                    focusedIndicatorColor = Color.Transparent,
+                    unfocusedIndicatorColor = Color.Transparent
+                ),
+//                onValueChange = messagingViewModel::messageTextChange,
+                onValueChange = singleChatRoomViewModel::messageTextChange,
+                trailingIcon = {
+                    IconButton(
+                        modifier = Modifier.padding(4.dp),
+                        onClick = {
+                            if (messageText.isNotEmpty()) {
+
+                                singleChatRoomViewModel.sendMessage(
+                                    roomUid = chatRoomUid,
+                                    senderUid = currentUserUid ,
+                                    textMessage = messageText
+                                )
+//                                messagingViewModel.sendMessage(
+//                                    roomUid = chatRoomUid,
+//                                    senderUid = currentUserUid,
+//                                    textMessage = messageText
+//                                )
+                            }
+
+
+                        },
+                        colors = IconButtonDefaults.iconButtonColors(
+                            contentColor = MaterialTheme.colorScheme.onPrimary,
+                            containerColor = MaterialTheme.colorScheme.primary
+                        ),
+                    ) {
+                        Icon(
+                            imageVector = Icons.AutoMirrored.Filled.Send,
+                            contentDescription = stringResource(
+                                id = R.string.send_message
+                            )
+                        )
+                    }
+                },
+                modifier = Modifier
+                    .padding(vertical = 10.dp)
+                    .fillMaxWidth(1.00f)
+            )
+
+        }
+
+
+    }
+}
